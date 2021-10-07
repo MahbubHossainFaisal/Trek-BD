63c0094de4a5eb2f2990948c86b77db20bb2e1b2
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-10-07 14:19:15 +0600

    URL params: get request for particular tour

--- app.js
+++ app.js
@@ -19,13 +19,35 @@ app.get('/api/v1/tours', (req,res) =>{
     })
 })
 
+//get request to get a particular tour using id as param
+app.get('/api/v1/tours/:id', (req,res) =>{
+    //converting string to number with * 1
+    const id = req.params.id * 1
+    const tour = tours.find(tour => tour.id === id)
+
+    if(!tour){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    })
+})
 //post request to create a new tour
 app.post('/api/v1/tours', (req,res)=>{
    const newID = tours[tours.length-1].id + 1;
+   //making a new object
    const newTour = Object.assign({id: newID}, req.body)
+   //adding it with older array tours
    tours.push(newTour)
 
    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err =>{
+       //reponse for post request
        res.status(201).json({
            status: 'success',
            data:{
