b37dd24a99bf48b99968cf35bfd1dc6e3dbac4a9
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-10-09 14:18:45 +0600

    Structuring routes,route handlers and server

---
+++ Controllers/tourController.js
@@ -0,0 +1,92 @@
+const fs = require('fs')
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`))
+
+
+//Route Handlers
+//get request to get all the tours
+exports.getAllTours = (req,res) =>{
+    
+    res.status(200).json({
+        status: 'success',
+        result: tours.length,
+        data: {
+            tours
+        }
+    })
+}
+
+//get request to get a particular tour using id as param
+exports.getTour = (req,res) =>{
+    //converting string to number with * 1
+    const id = req.params.id * 1
+    const tour = tours.find(tour => tour.id === id)
+
+    if(!tour){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    })
+}
+
+
+//post request to create a new tour
+exports.createTour = (req,res)=>{
+   const newID = tours[tours.length-1].id + 1;
+   //making a new object
+   const newTour = Object.assign({id: newID}, req.body)
+   //adding it with older array tours
+   tours.push(newTour)
+
+   fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err =>{
+       //reponse for post request
+       res.status(201).json({
+           status: 'success',
+           data:{
+               tour: newTour
+           }
+       })
+   })
+}
+
+//Patch request to update a tour
+exports.updateTour = (req,res) =>{
+    
+    if(req.params.id * 1 > tours.length){
+       return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+
+    // actually this is just a demo of patch req , not updating anything
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: 'Updated properties'
+        }
+    })
+}
+
+// delete a tour
+exports.deleteTour = (req,res) =>{
+    if(req.params.id * 1 > tours.length){
+       return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+
+    // actually this is just a demo of delete req , not deleting anything
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+}
--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const fs = require('fs')
+
 const morgan = require('morgan')
+const tourRouter = require('./Routes/tourRoutes.js')
+const userRouter = require('./Routes/userRoutes.js')
+
 const app = express()
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
 //middleware morgan
 
@@ -11,168 +13,8 @@ const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simpl
 app.use(morgan('dev'))
 app.use(express.json())
 
-
-
-//Route Handlers
-//get request to get all the tours
-const getAllTours = (req,res) =>{
-    
-    res.status(200).json({
-        status: 'success',
-        result: tours.length,
-        data: {
-            tours
-        }
-    })
-}
-
-//get request to get a particular tour using id as param
-const getTour = (req,res) =>{
-    //converting string to number with * 1
-    const id = req.params.id * 1
-    const tour = tours.find(tour => tour.id === id)
-
-    if(!tour){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        })
-    }
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour
-        }
-    })
-}
-
-
-//post request to create a new tour
-const createTour = (req,res)=>{
-   const newID = tours[tours.length-1].id + 1;
-   //making a new object
-   const newTour = Object.assign({id: newID}, req.body)
-   //adding it with older array tours
-   tours.push(newTour)
-
-   fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err =>{
-       //reponse for post request
-       res.status(201).json({
-           status: 'success',
-           data:{
-               tour: newTour
-           }
-       })
-   })
-}
-
-//Patch request to update a tour
-const updateTour = (req,res) =>{
-    
-    if(req.params.id * 1 > tours.length){
-       return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        })
-    }
-
-    // actually this is just a demo of patch req , not updating anything
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: 'Updated properties'
-        }
-    })
-}
-
-// delete a tour
-const deleteTour = (req,res) =>{
-    if(req.params.id * 1 > tours.length){
-       return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        })
-    }
-
-    // actually this is just a demo of delete req , not deleting anything
-    res.status(204).json({
-        status: 'success',
-        data: null
-    })
-}
-
-//Route Handlers for users
-
-const getAllUsers = (req,res) =>{
-    res.status(500).json({
-        status: 'error',
-        message: 'Route is not defined yet!'
-    })
-}
-const createUser = (req,res) =>{
-    res.status(500).json({
-        status: 'error',
-        message: 'Route is not defined yet!'
-    })
-}
-const getUser = (req,res) =>{
-    res.status(500).json({
-        status: 'error',
-        message: 'Route is not defined yet!'
-    })
-}
-
-const updateUser = (req,res) =>{
-    res.status(500).json({
-        status: 'error',
-        message: 'Route is not defined yet!'
-    })
-}
-
-const deleteUser = (req,res) =>{
-    res.status(500).json({
-        status: 'error',
-        message: 'Route is not defined yet!'
-    })
-}
-
-//Routes for tours
-const tourRouter = express.Router()
-tourRouter
-.route('/')
-.get(getAllTours)
-.post(createTour)
-
-
-
-
-tourRouter
-.route('/:id')
-.get(getTour)
-.patch(updateTour)
-.delete(deleteTour)
-
-
 app.use('/api/v1/tours',tourRouter)
-
-//Routes for users
-const userRouter = express.Router()
-
-userRouter
-.route('/')
-.get(getAllUsers)
-.post(createUser)
-
-userRouter
-.route('/:id')
-.get(getUser)
-.patch(updateUser)
-.delete(deleteUser)
-
 app.use('/api/v1/users',userRouter)
 
-//server
-const port = 5000;
-app.listen(port, ()=>{
-    console.log(`App is running on port ${port}...`)
-})
+
+module.exports = app
