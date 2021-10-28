ab307a6d23d510ff00679f1e1fb96b9165793028
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-10-28 16:12:27 +0600

    Aggregation pipeline and grouping

--- Routes/tourRoutes.js
+++ Routes/tourRoutes.js
@@ -7,12 +7,14 @@ const {
   updateTour,
   deleteTour,
   aliasTopTour,
+  getTourStats
 } = require('../Controllers/tourController');
 //  Routes for tours
 const router = express.Router();
 //  param middleware
 //router.param('id', checkID);
 router.route('/top-5-cheap-tour').get(aliasTopTour,getAllTours)
+router.route('/tour-stats').get(getTourStats)
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 module.exports = router;
